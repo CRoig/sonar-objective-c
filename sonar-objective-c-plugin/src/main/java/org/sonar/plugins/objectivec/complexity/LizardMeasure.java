39a1cf8ecfa6c68890a3b5cc79cf97908f6f463c
Tobias Raatiniemi <raatiniemi@gmail.com>
2018-03-02 05:59:28 +0100

    combine lizard metric and value into LizardMeasure

---
+++ sonar-objective-c-plugin/src/main/java/org/sonar/plugins/objectivec/complexity/LizardMeasure.java
@@ -0,0 +1,44 @@
+/**
+ * backelite-sonar-objective-c-plugin - Enables analysis of Objective-C projects into SonarQube.
+ * Copyright © 2012 OCTO Technology, Backelite (${email})
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.sonar.plugins.objectivec.complexity;
+
+import org.sonar.api.batch.measure.Metric;
+
+import java.io.Serializable;
+
+class LizardMeasure<T extends Serializable> {
+    private final Metric<T> metric;
+    private final T value;
+
+    private LizardMeasure(Metric<T> metric, T value) {
+        this.metric = metric;
+        this.value = value;
+    }
+
+    static <T extends Serializable> LizardMeasure of(Metric<T> metric, T value) {
+        return new LizardMeasure<>(metric, value);
+    }
+
+    public Metric<T> getMetric() {
+        return metric;
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
